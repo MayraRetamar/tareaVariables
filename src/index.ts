6c3d20ae67523aaa812b3d878d4e6a5bff19874e
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-04-27 19:30:52 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+let paso1: string = ("Encendemos el horno a 180°");
+let paso2: string = ("Ponemos en un bowl un kilo de harina y hacemos un hueco, agregamos sal, aceite de oliva y 500ml de agua tibia");
+let paso3: string = ("Aparte ponemos la levadura con 300ml de agua");
+let paso4: string = ("Después de activar la levadura la integramos a la preparación anterior");
+let paso5: string = ("Amasamos todo hasta que quede una masa lisa");
+let paso6: string = ("Formamos un bollo y dejamos leudar por 30 minutos o más");
+let paso7: string = ("Una vez que la masa este leudada poner en una placa enharinada y le hacemos unos cortes en la superficie");
+let paso8: string = ("Llevamos al horno y cocinamos por 50 minutos");
+let paso9: string = ("Una vez listo sacamos del horno y dejamos enfriar");
+let paso10: string = ("Ya lo podemos probar!!!");
+
+
+console.log (paso1);
+console.log (paso2);
+console.log (paso3);
+console.log (paso4);
+console.log (paso5);
+console.log (paso6);
+console.log (paso7);
+console.log (paso8);
+console.log (paso9);
+console.log (paso10);
+
+
+
